e307c53a5022c03432ea6056b7834b7a238592f5
Santhosh Chandrasekharan <csanthosh13@gmail.com>
2019-03-04 22:55:30 +0530

    Initial Commit

---
+++ TripCard/Trip.swift
@@ -0,0 +1,19 @@
+//
+//  Trip.swift
+//  TripCard
+//
+//  Created by Santosh Chandrasekharan on 02/03/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+struct Trip {
+    var tripId = ""
+    var city = ""
+    var country = ""
+    var featuredImage: UIImage?
+    var price:Int = 0
+    var totalDays:Int = 0
+    var isLiked = false
+}
---
+++ TripCard/TripCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  TripCollectionViewCell.swift
+//  TripCard
+//
+//  Created by Santosh Chandrasekharan on 02/03/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class TripCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var countryLabel: UILabel!
+    @IBOutlet var totalDaysLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    var isLiked:Bool = false {
+        didSet {
+            if isLiked {
+                likeButton.setImage(UIImage(named: "heartfull"), for: .normal)
+            } else {
+                likeButton.setImage(UIImage(named: "heart"), for: .normal)
+            }
+        }
+    }
+    var delegate: TripCollectionCellDelegate?
+    
+    @IBAction func likeButtonTapped(sender: AnyObject) {
+        delegate?.didLikeButtonPressed(cell: self)
+    }
+}
+
+protocol TripCollectionCellDelegate {
+    func didLikeButtonPressed(cell: TripCollectionViewCell)
+}
